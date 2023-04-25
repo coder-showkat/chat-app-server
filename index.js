3998e8abb5f361023bea51d2eafbcefb42b24cf0
coder-showkat <showkat.coder@gmail.com>
2023-04-25 06:37:18 +0600

    removing unnecessary pkg and codes

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
 const app = require("./app");
-const socket = require("socket.io");
 
 const port = process.env.PORT || 5000;
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running at ${port}`);
-})
-
-const io = socket(server, {
-    path:"/socket",
-    cors: {
-        origin: "https://chat-app-sam.netlify.app",
-        credentials: true,
-    }
-})
-
-
-io.on("connection", (socket)=>{
-
-    console.log("A user has connected!");
-
-    // listen for incoming message from the client side
-    socket.on("message", msg=>{
-        io.emit("message", msg);
-    });
-
-
-    // disconnect socket when the user disconnect
-    socket.on("disconnect", ()=>{
-        console.log("A user has disconnected!")
-    })
 })
