2cc3553a9aee50067c4f7c0b3461372d116c7132
coder-showkat <showkat.coder@gmail.com>
2023-05-15 14:29:45 +0600

    add middleware to fix cors error

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ const app = express();
 require("dotenv").config();
 require("./config/db");
 
-
 // middleware
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, HEAD, OPTIONS, POST, PUT, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-with, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 // all routes
 app.use("/user", userRouter);
@@ -21,19 +31,17 @@ app.use("/message", messageRouter);
 
 // home route
 app.get("/", (req, res) => {
-    res.send("<h1>Server is running properly</h1>");
+  res.send("<h1>Server is running properly</h1>");
 });
 
 // no route message
 app.use((req, res, next) => {
-    res.status(404).send({message: "No route matched"})
-})
-
+  res.status(404).send({ message: "No route matched" });
+});
 
 // server error message
 app.use((err, req, res, next) => {
-    res.status(500).send({message: "Server error"});
-})
-
+  res.status(500).send({ message: "Server error" });
+});
 
 module.exports = app;
